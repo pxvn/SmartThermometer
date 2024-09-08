8f53d5e48ffded8184bcca7805a569ad0baf20df
Pavan Kalsariya <161462414+pxvn@users.noreply.github.com>
2024-09-08 23:38:28 +0530

    CODE

--- code.cpp
+++ code.cpp
@@ -1,3 +1,19 @@
+/*
+MIT License
+
+Copyright (c) [2024] [PAVAN KALSARIYA]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+*/
+
+
 /* This code makes a contactless thermometer using an ESP32. It reads temperature from an infrared sensor and 
    shows it on a small screen. If the temperature gets too high, it warns you. 
    You can also see the temperature on your phone or computer because it connects to Wi-Fi and sends the data 
